7403deb797c9bdf27ab8036b967889dae28ff140
Stephen Hughes <steve.hughes@creative-technologies.co.uk>
2019-10-11 17:42:20 +0100

    Rotate emoji squares

--- src/components/EmojiSquare.tsx
+++ src/components/EmojiSquare.tsx
@@ -2,12 +2,25 @@ import React, { useState } from 'react';
 
 import './Bingo.scss';
 import "./App.css";
+import "./EmojiSquare.scss"
 
 const EmojiSquare: React.FC = () => {
-  const cheeseClick = () => { };
+  const emojis = ['🤷‍', '🧐', '😈', '💋', '👀', '🎅'];
+  const [index, setIndex] = useState<number>(0);
+  const incrementIndex = () => {
+    let newIndex = index + 1;
+    if (newIndex > 5) {
+      newIndex = 0;
+    }
+
+    setIndex(newIndex);
+  }
+
+  const currentEmoji = emojis[index];
+
   return (
-    <td onClick={cheeseClick}>
-      <span role="img" aria-label="shrug">🤷</span>
+    <td className="EmojiSquare" onClick={incrementIndex}>
+      <span role="img" aria-label="shrug">{currentEmoji}</span>
     </td>
   );
 };

b10fb08194b7b86637a5c83f0bea509d0dbcc5f3
Jack Allen <jackallensemail+git@gmail.com>
2020-01-25 21:45:04 +0000

    fuckit

--- src/constants/environment.ts
+++ src/constants/environment.ts
@@ -1,7 +1,7 @@
 const API_URL = process.env.REACT_APP_API_URL;
 
 const environment = {
-  API_URL: API_URL || 'https://localhost:5001'
+  API_URL: API_URL || 'https://api.cheeseybingo.co.uk'
 };
 
 console.log(environment);
